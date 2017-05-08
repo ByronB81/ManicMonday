8f0c5bcd87a27c0a853326251b685b1e28ac9552
Byron <byronblocker@gmail.com>
2017-05-08 10:21:27 -0700

    add in initial classes for person

---
+++ App.java
@@ -0,0 +1,24 @@
+public class App {
+  public static void main(String[] args) {
+    Person off1 = new Person();
+    off1.pName = "Susan";
+    off1.age = 34;
+    off1.hair = "brown";
+    off1.loud = true;
+    off1.met = false;
+
+    Person off2 = new Person();
+    off2.name = "Brian";
+    off2.age = 46;
+    off2.hair = "blonde";
+    off2.loud = false;
+    off2.met = true;
+
+    Person off3 = new Person();
+    off3.name = "Chris";
+    off3.age = 27;
+    off3.hair = "brown";
+    off3.loud = false;
+    off3.met = false;
+  }
+}
