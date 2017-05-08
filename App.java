be5f78408301c39d6b9ec867e8c6bd9789560d27
Byron <byronblocker@gmail.com>
2017-05-08 11:24:35 -0700

    add in branching

--- App.java
+++ App.java
@@ -3,22 +3,38 @@ import java.io.Console;
 public class App {
   public static void main(String[] args) {
     Console console = System.console();
-
-    System.out.println("how many K's can you pay?");
-    String strNum = console.readLine();
-    Integer maxAfford = Integer.parseInt(strNum);
-
     Person off1 = new Person("Susan",34,"brown",true, false,40);
     Person off2 = new Person("Brian",46,"blonde",false,true,75);
     Person off3 = new Person("Chris",27,"brown",false,false,55);
 
-  Person[] hatch = {off1,off2,off3};
+    Person[] hatch = {off1,off2,off3};
 
-    for  (Person coworker : hatch) {
-      if (coworker.afford(maxAfford)) {
+    System.out.println("Hello, would you like a full list or priced list?");
+    String navigate = console.readLine();
+
+    if (navigate.equals("full")||navigate.equals("Full")){
+      for (Person coworker : hatch) {
+        System.out.println("--------------");
         System.out.println(coworker.pName);
+        System.out.println(coworker.pAge);
+        System.out.println(coworker.pHair);
+        System.out.println(coworker.pLoud);
+        System.out.println(coworker.pMet);
+        System.out.println(coworker.pPrice);
       }
+    }else{
 
+    System.out.println("Okay, how many K's can you pay");
+    String strNum = console.readLine();
+    Integer maxAfford = Integer.parseInt(strNum);
+
+      for  (Person coworker : hatch) {
+        if (coworker.afford(maxAfford)) {
+          System.out.println("--------------");
+          System.out.println(coworker.pName);
+          System.out.println(coworker.pPrice);
+        }
+      }
 
 
       // System.out.println(coworker.pName);
